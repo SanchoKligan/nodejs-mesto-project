06f526fdaababade1e7730b2c4ac95c1fa786622
Александр Сысоев <alexgood5000@gmail.com>
2025-06-25 13:27:43 +0300

    Added schemes and models

---
+++ src/models/user.ts
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+// TODO: добавить интерфейс IUser
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  about: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 200,
+  },
+  avatar: {
+    type: String,
+    required: true,
+  },
+});
+
+export default mongoose.model('user', userSchema);
