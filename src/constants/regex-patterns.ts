6428ea7935f168828d8fa1ddddb558da7d98b333
Александр Сысоев <alexgood5000@gmail.com>
2025-06-30 19:39:38 +0300

    Fixed url regex

--- src/constants/regex-patterns.ts
+++ src/constants/regex-patterns.ts
@@ -1,4 +1,4 @@
-const AVATAR_REGEX = /^(https?:\/\/)(?:www\.)?([a-zA-Z0-9-]+\.){1,}[a-zA-Z0-9-]+\.[a-zA-Z]{2,}(\/[a-zA-Z0-9-._~:/?#[\]@!$&'()*+,;=]*)?#?$/;
+const AVATAR_REGEX = /^(https?:\/\/)(?:www\.)?(?!www\b)([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}(\/[a-zA-Z0-9-._~:/?#[\]@!$&'()*+,;=]*)?#?$/;
 const OBJECT_ID_REGEX = /^[a-f\d]{24}$/i;
 
 export {
